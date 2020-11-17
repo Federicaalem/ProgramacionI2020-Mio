9c9bc3c8cd72d41ace73768d7f5ecec82b071836
Federica Alem <Alemfederica00@outlook.com>
2020-11-17 10:07:46 -0300

    ff

--- Form1_Inscripcion.Designer.cs
+++ Form1_Inscripcion.Designer.cs
@@ -31,24 +31,26 @@
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnInscripcion = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.txtPago = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtContacto = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtApellido = new System.Windows.Forms.TextBox();
             this.txtDni = new System.Windows.Forms.TextBox();
-            this.btnOtro = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtNombre
             // 
             this.txtNombre.Location = new System.Drawing.Point(123, 81);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(201, 26);
+            this.txtNombre.Size = new System.Drawing.Size(231, 26);
             this.txtNombre.TabIndex = 0;
             // 
             // label1
@@ -62,11 +64,12 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.BackColor = System.Drawing.Color.LightSalmon;
+            this.groupBox1.Controls.Add(this.btnInscripcion);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.txtPago);
-            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtContacto);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.txtApellido);
@@ -74,22 +77,22 @@
             this.groupBox1.Controls.Add(this.txtNombre);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.groupBox1.Location = new System.Drawing.Point(30, 89);
+            this.groupBox1.Location = new System.Drawing.Point(94, 233);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(418, 423);
+            this.groupBox1.Size = new System.Drawing.Size(446, 444);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos del Subcriptor";
             // 
-            // button1
+            // btnInscripcion
             // 
-            this.button1.Location = new System.Drawing.Point(284, 383);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 34);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Suscribirse";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnInscripcion.Location = new System.Drawing.Point(284, 383);
+            this.btnInscripcion.Name = "btnInscripcion";
+            this.btnInscripcion.Size = new System.Drawing.Size(117, 34);
+            this.btnInscripcion.TabIndex = 6;
+            this.btnInscripcion.Text = "Inscribirse";
+            this.btnInscripcion.UseVisualStyleBackColor = true;
+            this.btnInscripcion.Click += new System.EventHandler(this.button1_Click);
             // 
             // label5
             // 
@@ -103,25 +106,33 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(42, 248);
+            this.label4.Location = new System.Drawing.Point(34, 248);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.Size = new System.Drawing.Size(81, 20);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Email:";
+            this.label4.Text = "Contacto:";
             // 
             // txtPago
             // 
+            this.txtPago.ForeColor = System.Drawing.SystemColors.InactiveCaption;
             this.txtPago.Location = new System.Drawing.Point(70, 351);
             this.txtPago.Name = "txtPago";
             this.txtPago.Size = new System.Drawing.Size(201, 26);
             this.txtPago.TabIndex = 4;
+            this.txtPago.Text = "$";
+            this.txtPago.Enter += new System.EventHandler(this.txtPago_Enter);
+            this.txtPago.Leave += new System.EventHandler(this.txtPago_Leave);
             // 
-            // txtEmail
+            // txtContacto
             // 
-            this.txtEmail.Location = new System.Drawing.Point(123, 245);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(201, 26);
-            this.txtEmail.TabIndex = 3;
+            this.txtContacto.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtContacto.Location = new System.Drawing.Point(123, 245);
+            this.txtContacto.Name = "txtContacto";
+            this.txtContacto.Size = new System.Drawing.Size(231, 26);
+            this.txtContacto.TabIndex = 3;
+            this.txtContacto.Text = "Telefono o Correo electronico";
+            this.txtContacto.Enter += new System.EventHandler(this.txtContacto_Enter);
+            this.txtContacto.Leave += new System.EventHandler(this.txtContacto_Leave);
             // 
             // label3
             // 
@@ -145,38 +156,56 @@
             // 
             this.txtApellido.Location = new System.Drawing.Point(123, 133);
             this.txtApellido.Name = "txtApellido";
-            this.txtApellido.Size = new System.Drawing.Size(201, 26);
+            this.txtApellido.Size = new System.Drawing.Size(231, 26);
             this.txtApellido.TabIndex = 1;
             // 
             // txtDni
             // 
             this.txtDni.Location = new System.Drawing.Point(123, 190);
+            this.txtDni.MaxLength = 8;
             this.txtDni.Name = "txtDni";
-            this.txtDni.Size = new System.Drawing.Size(201, 26);
+            this.txtDni.Size = new System.Drawing.Size(231, 26);
             this.txtDni.TabIndex = 2;
             // 
-            // btnOtro
+            // button2
             // 
-            this.btnOtro.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.btnOtro.Location = new System.Drawing.Point(344, 46);
-            this.btnOtro.Name = "btnOtro";
-            this.btnOtro.Size = new System.Drawing.Size(104, 37);
-            this.btnOtro.TabIndex = 6;
-            this.btnOtro.Text = "Otro";
-            this.btnOtro.UseVisualStyleBackColor = true;
-            this.btnOtro.Click += new System.EventHandler(this.btnOtro_Click);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button2.Location = new System.Drawing.Point(568, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 61);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Ir directamente a la base de Datos";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Programacion1Final.Properties.Resources.Mi_publicación__3_1;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(844, 217);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1_Inscripcion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 534);
-            this.Controls.Add(this.btnOtro);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::Programacion1Final.Properties.Resources.images;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(840, 689);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1_Inscripcion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Inscripcion";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -189,13 +218,14 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtPago;
-        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtContacto;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtApellido;
         private System.Windows.Forms.TextBox txtDni;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnOtro;
+        private System.Windows.Forms.Button btnInscripcion;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1_Inscripcion.cs
+++ Form1_Inscripcion.cs
@@ -12,32 +12,137 @@ namespace Programacion1Final
 {
     public partial class Form1_Inscripcion : Form
     {
-        DataTable Suscriptos = new DataTable();
+        DataTable DataSuscriptos = new DataTable();
+
         public Form1_Inscripcion()
         {
             InitializeComponent();
 
         }
 
+        //button incripcion
         private void button1_Click(object sender, EventArgs e)
         {
-            DataGlobal.Apellido = txtApellido.Text;
-            DataGlobal.Nombre = txtNombre.Text;
-            DataGlobal.Dni = txtDni.Text;
-            DataGlobal.Email = txtEmail.Text;
-            DataGlobal.Pago = decimal.Parse(txtPago.Text);
-            Form2_dtg FormularioDatos = new Form2_dtg();
-            FormularioDatos.ShowDialog();
+            #region comentario
+            //Suscribirse
 
-        }
+            //Los datos que entren por usuario se guardaran en las variables de la clase DataGlobal
+            //Dsp de eso instancio la clase Form2 para llamar a sus metodos y propiedades
 
-        private void btnOtro_Click(object sender, EventArgs e)
-        {
-            Form2_dtg Obj = new Form2_dtg();
-            Obj.OtroAdd();
-
+            #endregion
 
             
+                try
+                {
+                    DataGlobal.Apellido = txtApellido.Text;
+                    DataGlobal.Nombre = txtNombre.Text;
+                    DataGlobal.Dni = int.Parse(txtDni.Text);
+                    DataGlobal.Contacto = txtContacto.Text;
+                    DataGlobal.Pago = decimal.Parse(txtPago.Text);
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+
+
+
+            Form2_dtg Obj = new Form2_dtg();
+            //Obj.Calcular();
+            Obj.Grabar() ;
+            Limpiar();
+            MessageCreate mess = new MessageCreate();
+            mess.Show();
+           
+            //Obj.Grabar();
+            //Limpiar();
+            //MessageCreate messageCreate = new MessageCreate();
+            //messageCreate.ShowDialog();
+           
+          
+
+
         }
+
+
+        #region METODOS
+        public void Limpiar()
+        {
+            txtApellido.Clear();
+            txtDni.Clear();
+            txtContacto.Clear();
+            txtNombre.Clear();
+            txtPago.Clear();
+        }
+
+
+
+        #endregion
+
+        #region EVENTOS
+
+        #region Referencian en txtbox
+      
+        private void txtContacto_Enter(object sender, EventArgs e)
+        {//activo
+            if (txtContacto.Text == "Telefono o Correo electronico")
+            {
+                txtContacto.Text = "";
+                txtContacto.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void txtContacto_Leave(object sender, EventArgs e)
+        {//inactivo
+            if (txtContacto.Text == "")
+            {
+                txtContacto.Text = "Telefono o Correo electronico";
+                txtContacto.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPago_Enter(object sender, EventArgs e)
+        {
+            //activo
+            if (txtPago.Text == "$")
+            {
+                txtPago.Text = "";
+                txtPago.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtPago_Leave(object sender, EventArgs e)
+        {
+            //inactivo
+            if (txtPago.Text =="")
+            {
+                txtPago.Text = "$";
+                txtPago.ForeColor = Color.Silver;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Form2_dtg datagrid = new Form2_dtg();
+           datagrid.ShowDialog();
+        }
+
+
+
+
+
+        #endregion
+
+        #region Validacion de texboxs           
+
+
+        #endregion
+        #endregion
+
     }
+
+
 }
